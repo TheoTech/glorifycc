5d451d05565a36a9bd060501e5b66ac387c120ba
Jonathan Benhi <jonathanardewanta@gmail.com>
2016-09-30 11:42:17 -0700

    working on the admin layout

--- public/javascripts/songButton.js
+++ public/javascripts/songButton.js
@@ -1,70 +1,146 @@
-var setSong = function(id) {
+var addOrDeleteSong = function(id) {
     m.request({
-            method: 'POST',
-            url: '/songlist',
-            data: {
-                id: id
-            }
-        })
-        .then(function(data) {
-            if (data.url) {
-                window.location.href = data.url
-            } else {
-                info(data.msg)
-                console.log(data.msg)
-                console.log(data.status)
-                infoStatus(data.status)
-            }
-        })
-}
-var showInfo = function(elem) {
-    $(elem).click(function() {
-        console.log(infoStatus())
-        $('#info').show()
-        $("#info").fadeTo(1000, 500).slideUp(500, function() {
-            $(this).slideUp(500);
-        });
-    })
+        method: 'POST',
+        url: '/songlist',
+        data: {
+            id: id
+        }
+    }).then(function(res) {
+        if (res.url) {
+            window.location.href = res.url
+        } else {
+            inLibrary(res.inLibrary)
+        }
 
+    })
 }
-var info = m.prop()
-var infoStatus = m.prop('')
+
+var inLibrary = m.prop()
 var songButton = {
     view: function() {
-        return [m('#info.alert[style=display:none]', {
-                class: infoStatus()
-            }, info()),
-            m('button.btn.btn-success', {
-                config: function(elem, isInit, context) {
-                    if (!isInit) {
-                        showInfo(elem);
-                    }
-                },
-                onclick: function() {
-                    setSong(song._id)
+        return m('button.btn', {
+            class: function() {
+                if (_.includes(inLibrary(), song._id)) {
+                    return 'btn-danger'
+                } else {
+                    return 'btn-success'
                 }
-            }, 'Add to Library')
-        ]
+            }(),
+            onclick: function() {
+                addOrDeleteSong(song._id)
+            }
+        }, function() {
+            if (_.includes(inLibrary(), song._id)) {
+                return 'Delete from Library'
+            } else {
+                return 'Add to Library'
+            }
+        }())
     }
 }
 
 var transButton = {
     view: function() {
-        return [m('#info.alert[style=display:none]', {
-                class: infoStatus()
-            }, info()),
-            m('button.btn.btn-success', {
-                config: function(elem, isInit, context) {
-                    if (!isInit) {
-                        showInfo(elem);
-                    }
-                },
-                onclick: function() {
-                    setSong(translation._id)
+        return m('button.btn', {
+            class: function() {
+                if (_.includes(inLibrary(), translation._id)) {
+                    return 'btn-danger'
+                } else {
+                    return 'btn-success'
                 }
-            }, 'Add to Library')
-        ]
+            }(),
+            onclick: function() {
+                addOrDeleteSong(translation._id)
+            }
+        }, function() {
+            if (_.includes(inLibrary(), translation._id)) {
+                return 'Delete from Library'
+            } else {
+                return 'Add to Library'
+            }
+        }())
     }
 }
+// var songlistTable = {
+//     view: function() {
+//         return [
+//             m('table.table', [
+//                 m('thead', [
+//                     m('th', 'Title'),
+//                     m('th', 'Author'),
+//                     m('th')
+//                 ]),
+//                 m('tbody', [
+//                     songs().map((s) => {
+//                         return m('tr', [
+//                             m('td', [
+//                                 m('a', {
+//                                     href: '/songlist/' + s._id
+//                                 }, s.title)
+//                             ]),
+//                             m('td', s.author),
+//                             m('td', [
+//                                 m('button.btn', {
+//                                     class: function() {
+//                                         if (_.includes(inLibrary(), s._id)) {
+//                                             return 'btn-danger'
+//                                         } else {
+//                                             return 'btn-success'
+//                                         }
+//                                     }(),
+//                                     onclick: function() {
+//                                         addOrDeleteSong(s._id)
+//                                     }
+//                                 }, function() {
+//                                     if (_.includes(inLibrary(), s._id)) {
+//                                         return 'Delete from Library'
+//                                     } else {
+//                                         return 'Add to Library'
+//                                     }
+//                                 }())
+//                             ])
+//                         ])
+//                     })
+//                 ])
+//             ])
+//         ]
+//     }
+// }
+//
+//
+//
+//
+// var setSong = function(id) {
+//     m.request({
+//             method: 'POST',
+//             url: '/songlist',
+//             data: {
+//                 id: id
+//             }
+//         })
+//         .then(function(data) {
+//             if (data.url) {
+//                 window.location.href = data.url
+//             } else {
+//                 info(data.msg)
+//                 console.log(data.msg)
+//                 console.log(data.status)
+//                 infoStatus(data.status)
+//             }
+//         })
+// }
+// var showInfo = function(elem) {
+//     $(elem).click(function() {
+//         console.log(infoStatus())
+//         $('#info').show()
+//         $("#info").fadeTo(1000, 500).slideUp(500, function() {
+//             $(this).slideUp(500);
+//         });
+//     })
+//
+// }
+// var info = m.prop()
+// var infoStatus = m.prop('')
+
 m.mount(document.getElementById('song'), songButton)
 m.mount(document.getElementById('translation'), transButton)
