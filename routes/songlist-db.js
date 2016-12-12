3e004e72251eb58c554e37b187bcec54447baf1a
Chris <kirisu@gmail.com>
2016-12-12 15:49:32 -0800

    making publisher and year field optional. Also making auto-show lyrics form work.

--- routes/songlist-db.js
+++ routes/songlist-db.js
@@ -48,7 +48,6 @@ router.route('/add')
     .post(function(req, res, next) {
         req.checkBody('title', 'Title is empty').notEmpty();
         req.checkBody('author', 'Author is empty').notEmpty();
-        req.checkBody('year', 'Year is empty').notEmpty();
         var errors = req.validationErrors();
         if (errors) {
             res.send({
