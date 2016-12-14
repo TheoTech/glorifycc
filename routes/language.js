a05e061b5335cd7fe82ce0fc3adfb24879311ae2
Chris <kirisu@gmail.com>
2016-12-14 11:16:24 -0800

    adding access control to language database

--- routes/language.js
+++ routes/language.js
@@ -4,38 +4,40 @@ var express = require('express'),
     User = require('../models/user'),
     _ = require('lodash'),
     Playlist = require('../models/playlist'),
-    Language = require('../models/language');
+    Language = require('../models/language'),
+    passportFunction = require('../lib/passport');
 
-router.get('/', function(req, res, next) {
-    Language.find(function(err, languages) {
-        if (err) next(err);
-        res.render('language', {
-            languages: languages
-        })
-    })
-})
-
-router.post('/', function(req, res, next) {
-    var label = req.body.label;
-    var code = req.body.code;
-    Language.findOne({
-        code: code
-    }, function(err, language) {
-        if (err) next(err);
-        if (!language) {
-            var newLanguage = new Language({
-                label: label,
-                code: code
-            })
-            newLanguage.save(function(err) {
-                if (err) next(err);
-                res.redirect('/language');
-            })
-        } else {
-            req.flash('error_messages', 'Language already in the dabase');
-            res.redirect('/language');
-        }
-    })
-})
+router.all(passportFunction.loggedIn);
+router.route('/').all(passportFunction.loggedIn)
+  .get(function(req, res, next) {
+      Language.find(function(err, languages) {
+          if (err) next(err);
+          res.render('language', {
+              languages: languages
+          })
+      })
+  })
+  .post(function(req, res, next) {
+      var label = req.body.label;
+      var code = req.body.code;
+      Language.findOne({
+          code: code
+      }, function(err, language) {
+          if (err) next(err);
+          if (!language) {
+              var newLanguage = new Language({
+                  label: label,
+                  code: code
+              })
+              newLanguage.save(function(err) {
+                  if (err) next(err);
+                  res.redirect('/language');
+              })
+          } else {
+              req.flash('error_messages', 'Language already in the dabase');
+              res.redirect('/language');
+          }
+      })
+  });
 
 module.exports = router;
