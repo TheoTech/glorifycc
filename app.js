b9ba61460c4bfb1ab04685e0a2292ddec541b920
Dax Kerchner <daxkerchner@hotmail.com>
2016-12-16 16:33:10 -0800

    Add support page
    I added class=form-control to the PayPal code because without it the slate theme makes the select white-on-white.

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ var contact = require('./routes/contact');
 var discover = require('./routes/discover');
 var help = require('./routes/help');
 var legal = require('./routes/legal');
+var support = require('./routes/support');
 
 
 var mongoConfig = require('./lib/mongoConfig');
@@ -117,6 +118,7 @@ app.use('/contact', contact);
 app.use('/discover', discover);
 app.use('/help', help);
 app.use('/legal', legal);
+app.use('/support', support);
 
 app.get('/api', function(req, res, next) {
     app.render('songs-in-pdf', function(err, html) {
