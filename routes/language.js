c4ca69d506ac553214e1d2b5ede58983ea42b71b
Young <hogusong1@gmail.com>
2018-02-23 09:06:22 -0800

    upgrade language browser

--- routes/language.js
+++ routes/language.js
@@ -1,48 +1,64 @@
 var express = require('express'),
-    router = express.Router(),
-    Song = require('../models/song'),
-    User = require('../models/user'),
-    _ = require('lodash'),
-    Playlist = require('../models/playlist'),
-    Language = require('../models/language'),
-    passportFunction = require('../lib/passport');
+  router = express.Router(),
+  Song = require('../models/song'),
+  User = require('../models/user'),
+  Playlist = require('../models/playlist'),
+  Language = require('../models/language'),
+  passportFunction = require('../lib/passport');
 
-router.route('/').all(passportFunction.adminLoggedIn)
+router
+  .route('/')
+  .all(passportFunction.adminLoggedIn)
   .get(function(req, res, next) {
-      Language.find(function(err, languages) {
-          if (err) next(err);
-          res.render('songs/language', {
-              languages: languages
-          })
-      })
+    Language.find(function(err, languages) {
+      if (err) next(err);
+      res.render('songs/language', {
+        languages: languages
+      });
+    });
   })
   .post(function(req, res, next) {
-      var label = req.body.label;
-      var code = req.body.code;
-      Language.findOne({
-          code: code
-      }, function(err, language) {
-          if (err) next(err);
-          if (!language) {
-              var newLanguage = new Language({
-                  label: label,
-                  code: code
-              })
-              newLanguage.save(function(err) {
-                  if (err) next(err);
-                  res.redirect('/language');
-              })
-          } else {
-              Language.update({
-                  code: code
-              }, {
-                  label: label
-              }, {}, function (err, updated) {
-                  if (err) next(err);
-                  res.redirect('/language');
-              });
-          }
-      })
+    req.checkBody('label', 'Language label is required').notEmpty();
+    req.checkBody('code', 'Langeage code is required').notEmpty();
+    var label = req.body.label;
+    var code = req.body.code;
+    var errors = req.validationErrors();
+    if (errors) {
+        req.flash('error_messages', 'Invalid data entry');
+        res.redirect('/language');
+    } else {
+        Language.findOne(
+        {  code: code  },
+        function(err, language) {
+            if (err) next(err);
+            if (!language) {
+                var newLanguage = new Language({
+                    label: label,
+                    code: code
+                });
+                newLanguage.save(function(err) {
+                    if (err) next(err);
+                    req.flash('success_messages', 'Saved successfully!!!');
+                    res.redirect('/language');
+                });
+            } else {
+                Language.update(
+                    {
+                    code: code
+                    },
+                    {
+                    label: label
+                    },
+                    {},
+                    function(err, updated) {
+                    if (err) next(err);
+                    req.flash('success_messages', 'Updated successfully!!!');
+                    res.redirect('/language');
+                    }
+                );
+            }
+        });
+    }
   });
 
 module.exports = router;
